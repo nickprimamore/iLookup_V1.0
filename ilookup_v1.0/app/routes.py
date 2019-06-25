567ce982cefa932d82a0240870c829fe5d46ef20
Krish Patel <kkpatel@bu.edu>
2019-06-25 11:18:48 -0400

    UI Enhancements + Comments

--- ilookup_v1.0/app/routes.py
+++ ilookup_v1.0/app/routes.py
@@ -6,20 +6,30 @@ import json
 import boto3
 
 @app.route('/', methods=['GET', 'POST'])
+#This function gathers all the data from the SQL tables to generate the search filters
 def search():
 	clients = Client.query.all()
 	products = Product.query.all()
 	releases = Product_Release.query.all()
 	clusters = Cluster.query.all()
+	environments = []
+	regions = []
+	#Remove duplicate values such as "dev" and "qa"
+	for cluster in clusters:
+	 	if cluster.environment not in environments:
+		 	environments.append(cluster.environment)
+		if cluster.region not in regions:
+			regions.append(cluster.region)
 	components = Component.query.all()
-	return render_template('result.html', clientsQ=clients, productsQ=products, releasesQ=releases, clustersQ=clusters, componentsQ=components)
+	#Renders the Result.html file which extends Search.html which extends Layout.html
+	return render_template('result.html', clientsQ=clients, productsQ=products, releasesQ=releases, clustersQ=clusters, componentsQ=components, environmentsQ=environments, regionsQ=regions)
 
 @app.route('/result', methods=['GET','POST'])
+#This function communicates with the HTML and gathers the responses in order to load the table data.
 def result():
+	#Parsing through the Immutable Multi Dictionary in order to get values that can be used within SQL
 	data = request.form.keys()
-	print(data)
 	for values in data:
 		stringified = values
 		objectified = json.loads(values)
-		print(objectified)
 	return render_template('result.html')
