93dac633b5c3c4f4b4b2a89d2a04fc9136200e0e
Nick Primamore <lancernp@gmail.com>
2019-06-26 09:21:19 -0400

    Dates added to Ajax request

--- ilookup_v1.0/config.py
+++ ilookup_v1.0/config.py
@@ -6,6 +6,6 @@ class Config(object):
 	SECRET_KEY = os.environ.get("SECRET_KEY") or "SECRET-KEY"
 	#SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')'sqlite:///' + os.path.join(basedir,'app.db')
 
-	SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Sj93299347!@127.0.0.1:3306/localdb"
+	SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Allrise99!@127.0.0.1:3306/localdb"
 
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
