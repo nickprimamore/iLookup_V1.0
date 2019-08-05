c39669e2f3c46fec0ee26aef5bc785ccdeae53d0
sayefiqb <sayefiqbal007@gmail.com>
2019-08-05 13:08:53 -0400

    nothing changed

--- ilookup_v1.0/cronjob.py
+++ ilookup_v1.0/cronjob.py
@@ -1,2 +1,11 @@
+#!/usr/bin/env python3
 from awsdata import AWSData
 from checkData import CheckAWSData
+from app import db
+
+awsdata = AWSData()
+awsdata.newMainFunction()
+db.session.commit()
+checkData = CheckAWSData()
+checkData.checkData()
+db.session.commit()
